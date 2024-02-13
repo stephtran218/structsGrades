57051278f66dcbc4ff9c8d6fbed81973566f61f8
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-02-13 10:30:45 -0800

    2/13

--- structs/structs/main.swift
+++ structs/structs/main.swift
@@ -6,6 +6,34 @@
 //
 
 import Foundation
+import CSV
 
-print("Hello, World!")
+//This is a safety parameter that prevents errors from crashing the code
+do{
+    let stream = InputStream(fileAtPath: "/Users/studentam/Desktop/grades.csv" )
+    let csv = try CSVReader(stream: stream!)
+    
+    while let row = csv.next(){
+        manageData (row)
+    }
+}
+catch{
+    print("There was an error trying to read the file")
+}
+var studentInfo: [String] = []
 
+struct classInfo {
+    var fullName: String
+    var grades: [String]
+    var finalGrade: Double
+}
+
+func manageData( _ studentInfo: [String]){
+    var tempGrades: [grades]
+    
+    for i in grades.indices{
+        if i == 0{
+            fullNames.append(studentInfo[0])
+        }
+    }
+}
